1eb5457926590f57369d3101c04ced7d7f94e578
Rohit Saxena <sunnybly007@gmail.com>
2022-03-08 12:18:45 +0530

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const App=()=>{
+    return <SafeAreaView>
+        <Text>Hello from Grassy</Text>
+    </SafeAreaView>
+}
+export default App;
