0462946f9fd2f221833c97911f01e76c0c06e4c7
Rohit Saxena <sunnybly007@gmail.com>
2022-03-08 13:15:32 +0530

    SafeAreaProvide, Vector Icons and react native elements added

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const App=()=>{
-    return <SafeAreaView>
+    return <SafeAreaProvider>
         <Text>Hello from Grassy</Text>
-    </SafeAreaView>
+    </SafeAreaProvider>
 }
 export default App;
